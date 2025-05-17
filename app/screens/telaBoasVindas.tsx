5fa5e96e8ab58766be49f65174fd9985ddf8b0dc
Elen Freire <elenfreire63@gmail.com>
2025-05-17 16:30:27 -0300

    layout setup

--- app/screens/telaBoasVindas.tsx
+++ app/screens/telaBoasVindas.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { SafeAreaView, View, ScrollView, StyleSheet, } from "react-native";
-export default (props) => {
+import { SafeAreaView, View, ScrollView, StyleSheet, ImageBackground, } from "react-native";
+export default (props: any) => {
 	return (
 		<SafeAreaView style={styles.container}>
 			<ScrollView  style={styles.scrollView}>
-			    <View style={styles.column}>
-			        <ImageBackground
-			            source={require('"..\app\assets\images\Layout\deep-0ECAhyBYxhE-unsplash (1).png"')}
-			            style={styles.view}
-			        >
-			    </view>
+				<View style={styles.column}>
+					<ImageBackground
+						source={require('"..\app\assets\images\Layout\deep-0ECAhyBYxhE-unsplash (1).png"')}
+						style={styles.view}
+					>
+					</ImageBackground>
+				</View>
 			</ScrollView>
 		</SafeAreaView>
 	)
@@ -22,5 +23,14 @@ const styles = StyleSheet.create({
 	scrollView: {
 		flex: 1,
 		backgroundColor: "#FFFFC6",
+	},
+	column: {
+		flexDirection: "column",
+		flex: 1,
+	},
+	view: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center",
 	},
 });
