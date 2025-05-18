01eb9122d4c269bff1929c6a055ebec21389f96b
Elen Freire <elenfreire63@gmail.com>
2025-05-18 02:30:40 -0300

    Fix

---
+++ app/navigation/index.tsx
@@ -0,0 +1,15 @@
+// app/navigation/index.tsx
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ app/services/firebaseConfig.js
@@ -0,0 +1,18 @@
+// /app/services/firebaseConfig.js
+
+import Constants from 'expo-constants';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
+};
+
+// Inicializa o app Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
