cb42b3952d794c1387ea881cfb1107abd22eb4df
Elen Freire <elenfreire63@gmail.com>
2025-06-13 13:59:46 -0300

    fixes index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
 import { Image, ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Defs, FeBlend, FeColorMatrix, FeComposite, FeFlood, FeOffset, Filter, G, Path, Svg } from 'react-native-svg';
+import { Defs, FeBlend, FeColorMatrix, FeComposite, FeFlood, FeGaussianBlur, FeOffset, Filter, G, Path, Svg } from 'react-native-svg';
 
 // Componente de mensagem de boas-vindas
 function Mensagemdeboasvindas() {
@@ -58,55 +58,77 @@ function TelaInicial() {
                 </View>
                 {/* Container de introdução */}
                 <View style={styles.telainicialintroducaoContainer}>
-                    {/* SVG decorativo de fundo */}
-                    <Svg style={styles.vector} width="412" height="706" viewBox="0 0 412 706" fill="none" >
-                        {/* ...SVG de fundo... */}
-                    </Svg>
+                    <View style={styles.vectorContainer2}>
+                        {/* SVG decorativo de fundo */}
+                        <Svg style={styles.vector2} width="640" height="640" viewBox="0 0 412 706" fill="none">
+                            <G filter="url(#filter0_dddi_24_59)">
+                                <Path fillRule="evenodd" clipRule="evenodd" d="M168.105 34.9664C217.732 27.6977 262.161 63.2085 309.525 82.065C373.504 107.536 453.756 102.357 495.06 165.133C536.883 228.699 534.032 321.411 518.47 399.633C503.544 474.65 463.445 541.354 411.001 587.636C363.736 629.347 301.33 627.475 243.559 642.242C190.395 655.83 133.48 700.968 85.5026 670.833C36.2972 639.927 49.0497 548.303 13.5326 497.762C-20.5811 449.218 -98.6854 448.549 -113.266 387.984C-127.494 328.88 -83.0334 272.144 -52.858 221.976C-26.8279 178.7 11.0715 151.395 47.674 120.417C86.5108 87.5477 120.601 41.924 168.105 34.9664Z" fill="white" />
+                                <Path d="M171.583 58.7129C189.656 56.0658 208.098 60.952 229.915 70.6494C240.832 75.502 251.671 81.1158 263.594 87.1455C274.495 92.6588 286.159 98.4428 298.229 103.386L300.647 104.363C318.355 111.413 336.791 116.189 354.151 120.312C372.041 124.561 388.23 128.006 404.046 132.641C434.598 141.594 458.206 153.54 474.253 177.19L475.011 178.325C510.727 232.609 510.3 315.188 495.64 391.329L494.931 394.949C481.16 464.165 444.283 525.799 396.257 568.633L395.121 569.641C375.764 586.723 352.771 595.474 325.853 601.65C312.266 604.768 298.325 607.1 283.345 609.699C269.563 612.09 255.014 614.691 240.515 618.262L237.615 618.989C222.472 622.86 207.533 628.845 193.921 634.538C179.773 640.455 167.262 645.957 154.8 650.231C130.185 658.673 113.264 659.566 98.9473 650.928L98.2676 650.51C83.3009 641.109 75.5229 620.916 67.8057 586.421C64.334 570.903 60.7457 552.284 55.9395 535.26C51.2046 518.488 44.7112 500.922 34.1992 485.453L33.1689 483.963C11.9745 453.803 -23.1264 438.478 -45.8037 426.255C-70.7523 412.807 -84.8583 401.938 -89.708 383.262L-89.9326 382.366C-95.211 360.439 -90.0523 337.453 -77.999 311.852C-65.9566 286.273 -49.6376 262.931 -33.8203 236.877L-32.292 234.347C-9.59954 196.62 22.6064 172.827 59.585 141.766L63.1787 138.736C73.5495 129.959 83.4953 120.424 92.8018 111.505C102.306 102.397 111.16 93.9151 120.164 86.2773C138.037 71.1166 154.003 61.5175 170.783 58.8359L171.583 58.7129Z" stroke="#744F38" strokeWidth="45" />
+                            </G>
+                            <Defs>
+                                <Filter id="filter0_dddi_24_59" x="-135.4" y="0" width="725.4" height="705.4" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <FeFlood floodOpacity="0" result="BackgroundImageFix" />
+                                    <FeColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <FeOffset dx="-19" dy="24" />
+                                    <FeGaussianBlur stdDeviation="0.2" />
+                                    <FeComposite in2="hardAlpha" operator="out" />
+                                    <FeColorMatrix type="matrix" values="0 0 0 0 0.729412 0 0 0 0 0.294118 0 0 0 0 0.188235 0 0 0 1 0" />
+                                    <FeBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_24_59" />
+                                    <FeColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <FeOffset dx="61" dy="-34" />
+                                    <FeComposite in2="hardAlpha" operator="out" />
+                                    <FeColorMatrix type="matrix" values="0 0 0 0 0.591925 0 0 0 0 0.625961 0 0 0 0 0.555541 0 0 0 1 0" />
+                                    <FeBlend mode="normal" in2="effect1_dropShadow_24_59" result="effect2_dropShadow_24_59" />
+                                    <FeColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <FeOffset dx="-10" dy="4" />
+                                    <FeComposite in2="hardAlpha" operator="out" />
+                                    <FeColorMatrix type="matrix" values="0 0 0 0 0.964706 0 0 0 0 0.831373 0 0 0 0 0.631373 0 0 0 1 0" />
+                                    <FeBlend mode="normal" in2="effect2_dropShadow_24_59" result="effect3_dropShadow_24_59" />
+                                    <FeBlend mode="normal" in="SourceGraphic" in2="effect3_dropShadow_24_59" result="shape" />
+                                    <FeColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <FeOffset dx="-94" dy="63" />
+                                    <FeComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+                                    <FeColorMatrix type="matrix" values="0 0 0 0 0.380392 0 0 0 0 0.745098 0 0 0 0 0.670588 0 0 0 1 0" />
+                                    <FeBlend mode="normal" in2="shape" result="effect4_innerShadow_24_59" />
+                                </Filter>
+                            </Defs>
+                        </Svg>
 
-                    {/* Frase de destaque */}
-                    <Text style={styles.organizesuavidafinanceiradeformainteligente}>
-                        Organize sua vida financeira de forma inteligente!
-                    </Text>
-
-                    {/* Imagem de carteira */}
+                        {/* Frase de destaque */}
+                        <View style={styles.svgTextWrapper}>
+                            <Text style={styles.svgText}>
+                                Organize sua vida{'\n'}financeira de forma inteligente!
+                            </Text>
+                        </View>
+                    </View>
+                    {/* Imagem de contas */}
                     <Image
-                        source={require('../../src/assets/images/carteira.png')}
-                        style={{ width: 200, height: 114, alignSelf: 'center', marginVertical: 16 }}
+                        source={require('../../src/assets/images/conta.png')}
+                        style={{ width: 400, height: 300, alignSelf: 'center', marginVertical: 16, transform: [{ rotate: '-15deg' }] }}
                         resizeMode="contain"
                     />
                 </View>
-
-                {/* Botão de Login */}
-                <TouchableOpacity style={styles.button} onPress={() => router.push('/auth/login')}>
-                    <Text style={styles._button}>
-                        Login
-                    </Text>
-                    <View style={styles.arrowright}>
-                        {/* Ícone de seta para a direita */}
-                        <Svg style={styles.icon} width="13" height="13" viewBox="0 0 13 13" fill="none" >
-                            <Path d="M1.83333 6.49992H11.1667M11.1667 6.49992L6.5 1.83325M11.1667 6.49992L6.5 11.1666" stroke="#070000" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
-                        </Svg>
+                <View style={{ alignItems: "center", width: "100%", marginTop: 32 }}>
+                    {/* Botão de Login */}
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity
+                            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+                            onPress={() => router.push('/auth/login')}
+                        >
+                            <Text style={styles.button}>Login</Text>
+                        </TouchableOpacity>
                     </View>
-                </TouchableOpacity>
 
-                {/* Linha decorativa separando os botões */}
-                <Svg style={styles.line1} width="270" height="18" viewBox="0 0 270 18" fill="none" >
-                    {/* ...SVG da linha... */}
-                </Svg>
-
-                {/* Botão de cadastro */}
-                <TouchableOpacity style={styles.__button} onPress={() => router.push('/auth/cadastro')}>
-                    <Text style={styles.___button}>
-                        Cadastre-se
-                    </Text>
-                    <View style={styles._arrowright}>
-                        {/* Ícone de seta para a direita */}
-                        <Svg style={styles.icon} width="12" height="13" viewBox="0 0 12 13" fill="none" >
-                            <Path d="M1.33333 6.50004H10.6667M10.6667 6.50004L6 1.83337M10.6667 6.50004L6 11.1667" stroke="#070000" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
-                        </Svg>
+                    {/* Botão de Cadastro */}
+                    <View style={[styles.buttonContainer, { marginTop: 16 }]}>
+                        <TouchableOpacity
+                            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+                            onPress={() => router.push('/auth/cadastro')}
+                        >
+                            <Text style={styles.button}>Cadastre-se</Text>
+                        </TouchableOpacity>
                     </View>
-                </TouchableOpacity>
-
+                </View>
                 {/* Logo extra no rodapé */}
                 <ImageBackground
                     style={styles.logo_icon}
@@ -159,7 +181,7 @@ const styles = StyleSheet.create({
     ao: {
         position: "absolute",
         flexShrink: 0,
-        top: 120,
+        top: 365,
         width: 426,
         height: 130,
         textAlign: "center",
@@ -174,14 +196,14 @@ const styles = StyleSheet.create({
     bemvindo: {
         position: "absolute",
         flexShrink: 0,
-        top: 100,
+        top: 310,
         width: 426,
         height: 130,
         textAlign: "center",
         color: "rgba(116, 79, 56, 1)",
         fontFamily: "Anonymous Pro",
-        fontSize: 60,
-        fontWeight: "400",
+        fontSize: 65,
+        fontWeight: "500",
         textShadowColor: "rgba(24, 16, 10, 1)",
         textShadowOffset: { width: -2, height: 2 },
         textShadowRadius: 0.5
@@ -189,10 +211,10 @@ const styles = StyleSheet.create({
     logo: {
         position: "relative",
         flexShrink: 0,
-        top: 180,
+        top: 55,
         width: 383,
         height: 187,
-        marginLeft: 50,
+        marginLeft: 70,
         marginBottom: 100,
         marginTop: 100,
     },
@@ -202,25 +224,46 @@ const styles = StyleSheet.create({
         alignItems: "center",
         width: "100%",
     },
-    organizesuavidafinanceiradeformainteligente: {
-        marginTop: 16,
+    vectorContainer2: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40,
+        marginBottom: 24,
+        width: '100%',
+        height: 640, 
+        position: 'relative',
+    },
+    vector2: {
+        width: 640, 
+        height: 640, 
+    },
+
+    buttonContainer: {
+        position: "relative",
+        flexShrink: 0,
+        height: 71,
+        width: 293,
+        backgroundColor: "rgba(255, 255, 255, 1)",
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+        shadowOffset: {
+            width: 6,
+            height: 6
+        },
+        shadowRadius: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        columnGap: 20,
+        padding: 12,
+        borderRadius: 8
+    },
+    button: {
         textAlign: "center",
         color: "rgba(0, 0, 0, 1)",
         fontFamily: "Anonymous Pro",
-        fontSize: 24,
+        fontSize: 32,
         fontWeight: "400",
-    },
-    button: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#fff",
-        padding: 12,
-        borderRadius: 8,
-        marginTop: 32,
-        shadowColor: "rgba(0, 0, 0, 0.25)",
-        shadowOffset: { width: 6, height: 6 },
-        shadowRadius: 0,
+        lineHeight: 32,
     },
     _button: {
         fontSize: 32,
@@ -271,6 +314,27 @@ const styles = StyleSheet.create({
         flexShrink: 0,
         width: 383,
         height: 187,
+    },
+    svgTextWrapper: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: 500, 
+        height: 460, 
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 1,
+        paddingHorizontal: 16,
+    },
+    svgText: {
+        marginTop: 240,
+        right: 55,
+        textAlign: "center",
+        color: "rgba(0, 0, 0, 1)",
+        fontFamily: "Anonymous Pro",
+        fontSize: 40,
+        fontWeight: "400",
+
     },
 });
 
